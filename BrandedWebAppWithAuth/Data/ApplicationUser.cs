be58f12a6bb328710722725c5a30aca05ef1fbd4
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-05-19 15:05:49 -0500

    Updating the register page

--- BrandedWebAppWithAuth/Data/ApplicationUser.cs
+++ BrandedWebAppWithAuth/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace BrandedWebAppWithAuth.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        //extending IdentityUser with custom properties
+        public required string FirstName { get; set; } 
+        public required string LastName { get; set; }
     }
 
 }
